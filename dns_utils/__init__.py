a60d7537de27fbc24808a18f5375cbdf2c5c3bfa
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2026-03-07 09:45:05 +0330

    Update build

---
+++ dns_utils/__init__.py
@@ -0,0 +1,21 @@
+__all__ = []
+
+
+def _try_export(name, from_module=None):
+    try:
+        if from_module:
+            mod = __import__(f"dns_utils.{from_module}", fromlist=[name])
+            obj = getattr(mod, name)
+        else:
+            mod = __import__(f"dns_utils.{name}", fromlist=[name])
+            obj = getattr(mod, name, mod)
+        globals()[name] = obj
+        __all__.append(name)
+    except Exception:
+        pass
+
+
+_try_export("DnsPacketParser")
+_try_export("ARQ")
+_try_export("DNSBalancer")
+_try_export("PingManager")
